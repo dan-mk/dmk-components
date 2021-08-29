9f5b425cca75298b5e7a651b5ec6b8f583255302
Daniel MK <danielmk98@hotmail.com>
2021-08-28 23:42:10 -0300

    First version of the DmkSelect component: simple select with the ability to search for options

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DmkSelect } from './DmkSelect';
+
+let selected = { value: '1', text: 'Resultado 1' };
+let optionsUrl = 'http://localhost:8080/?query=';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div id="container"><DmkSelect selected={selected} optionsUrl={optionsUrl} /></div>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
